e8e1d6032bf271f251cf8e3360833f8923b6934d
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-02-07 15:15:51 -0600

    45

--- basic/sketch.js
+++ basic/sketch.js
@@ -1,34 +1,15 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
+// basic interaction demo
 
-// global variables
-let gear;
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  creativeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255);
 
-  image(gear, 0, 0);
+}
 
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
+function keyPressed() {
   noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+  fill(random(255), random(255), random(255), random(255))
+  ellipse(random(0, width), random(0, height), random(50, 200), random(50, 200));
 }
