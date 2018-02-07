5372613ed88e46668df1abc7e6dfdf4ecfdcccf5
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-02-07 15:19:23 -0600

    added

--- basic/sketch.js
+++ basic/sketch.js
@@ -10,6 +10,6 @@ function draw() {
 
 function keyPressed() {
   noStroke();
-  fill(random(255), random(255), random(255), random(255))
+  fill(random(255), random(255), random(255), random(255));
   ellipse(random(0, width), random(0, height), random(50, 200), random(50, 200));
 }
