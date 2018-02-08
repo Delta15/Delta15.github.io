62d029ecbc252e6ef3c471dc633c64fcf14cd112
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-02-08 14:46:45 -0600

    addded deviceShaken()

--- basic/sketch.js
+++ basic/sketch.js
@@ -1,7 +1,7 @@
 // basic interaction demo
 
 function setup() {
-  creativeCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
@@ -12,4 +12,19 @@ function keyPressed() {
   noStroke();
   fill(random(255), random(255), random(255), random(255));
   ellipse(random(0, width), random(0, height), random(50, 200), random(50, 200));
+}
+
+function mouseClicked() {
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  rect(mouseX, mouseY, random(50, 200), random(50, 200));
+}
+
+function deviceShaken() {
+  fill(0);
+  textFont("Arial");
+  textAlign(CENTER);
+  textSize(40);
+  text("Text Sample", random(width), random(height));
+
 }
