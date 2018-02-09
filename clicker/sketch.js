dc2ea45fcefed2a1470f71d1e88cf3e7affa557e
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-02-09 14:55:08 -0600

    delete and fixed

---
+++ clicker/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  document.addEventListener("contextmenu", event => event.preventDefault());
+}
+
+function draw() {
+
+}
+
+function mouseClicked(){
+  if (mouseButton == LEFT){
+    noStroke();
+    fill(random(255), random(255), random(255), random(255));
+    ellipse(mouseX, mouseY, random(50, 200), random(50, 200));
+  }
+  else if (mouseButton == RIGHT) {
+    noStroke();
+    fill(random(255),random(255),random(255),random(255));
+    ellipse(mouseX,mouseY,random(50, 150),random(50,150));
+  }
+}
+
+function keyPressed(){
+  if (key === "e" || key === "E"){
+    background(255);
+  }
+}
