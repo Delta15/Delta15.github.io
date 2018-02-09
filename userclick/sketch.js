58cce2be5b4f696b9a2a1b92ecc8840897c29053
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-02-09 14:39:49 -0600

    change

--- userclick/sketch.js
+++ userclick/sketch.js
@@ -4,26 +4,27 @@
 
 function setup() {
   createCanvas(windowWidth,windowHeight,);
-  document.addEventListener("contextmenu", event => event.preventDefault())
+  document.addEventListener("contextmenu", event => event.preventDefault());
 }
 
 function draw() {
 
 }
+
 function mouseClicked() {
-  if (mouseButton == LEFT){
+  if (mouseButton === LEFT){
     noStroke();
     fill(random(255),random(255),random(255),random(255));
     rect(mouseX,mouseY,random(50, 150),random(50, 150));
   }
   else if (mouseButton === RIGHT) {
-    ellipse(mouseX,mouseY,w,[h])
+    noStroke();
+    fill(random(255),random(255),random(255),random(255));
+    ellipse(mouseX,mouseY,random(50, 150),random(50,150));
   }
 }
 
 function keyPressed() {
-  if (key === "e" || key === "E") {
-    background(255);
-  }
-  //else if (key === "b" || key === "B")
+  if (key === "e" || key === "E"){
+  background(0);
 }
