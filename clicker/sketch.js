2ca7bf5f260bd7839ef96edff5d64eff9af9f2c1
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-02-16 15:18:22 -0600

    fixed

--- clicker/sketch.js
+++ clicker/sketch.js
@@ -1,49 +1,64 @@
 //Clicker
 //McRaven Tuazon
 //2/15/2018
-
-let fr = 60;
-let time = 0;
-let endBox = TRUE;
+let drawBox;
+let drawBox2;
+let drawBox3;
+let drawBox4;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  drawBox = true;
+  drawBox2 = true;
+  drawBox3 = true;
+  drawBox4 = true;
   document.addEventListener("contextmenu", event => event.preventDefault());
 }
 
 function draw() {
-  background(255,105,180)
-  // background(random(255), random(255), random(255), random(255));
-  // fill(random(255), random(255), random(255), random(255));
-  noFill();
-  stroke(0);
-  rotateY(millis() / 800);
-  //rotateX(millis() / 900);
-  box(300, 300);
-  rotateX(millis() / 800);
-  box(200, 200);
-  rotateZ(millis() / 800);
-  box(100, 100);
-  fill(255);
-  noStroke();
-  sphere(40);
+  background(255,105,180);
+  boxStart();
+  boxStart2();
+  boxStart3();
+}
+
+function boxStart(){
+  if (drawBox === true){
+    noFill();
+    stroke(0);
+    rotateY(millis() / 800);
+    box(300, 300);
+  }
+}
+
+function boxStart2(){
+  if (drawBox2 === true){
+    noFill();
+    stroke(0);
+    rotateX(millis() / 800)
+    box(200, 200);
+  }
+}
+
+function boxStart3(){
+  if (drawBox3 === true){
+    noFill();
+    stroke(0);
+    rotateZ(millis() / 800)
+    box(100, 100);
+  }
 }
 
 function mouseClicked(){
   if (mouseButton == LEFT){
-    fill(random(255), random(255), random(255), random(255));
-    ellipse(mouseX, mouseY, random(50, 200), random(50, 200));
-  }
-  else if (mouseButton == RIGHT) {
-    noStroke();
-    fill(random(255),random(255),random(255),random(255));
-    rect(mouseX,mouseY,random(50, 150),random(50,150));
+    drawBox = false();
   }
 }
 
 function keyTyped(){
   if (key === "e" || key === "E"){
     background(255);
+    clear();
   }
 }
 
