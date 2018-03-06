33c872622853a353821c932ad5b2917ee3a61a2e
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-03-06 15:00:49 -0600

    added text

--- State Variables Assignment/sketch.js
+++ State Variables Assignment/sketch.js
@@ -1,7 +1,18 @@
+let gameOpening;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  gameOpening = true;
 }
 
 function draw() {
-  background(0);
+  background(255);
+  gameScreen();
+}
+
+function gameScreen(){
+  fill(0);
+  noStroke();
+  rectMode(CENTER);
+  rect(width/2,height/2,windowWidth,200);
 }
