a4bba4d56bc2a98f507c1df1601d1c96babb95c0
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-03-14 14:44:04 -0600

    added

--- Translation demo/sketch.js
+++ Translation demo/sketch.js
@@ -1,17 +1,34 @@
+let clockSize;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  if (width > height){
+    clockSize = height + 0.9;
+  }
+  else{
+    clockSize = width + 0.9;
+  }
 }
 
 function draw() {
   background(255);
   push();//save
+  // translate(width/2,height/2);
+  // rotate(frameCount);
+  // fill(0);
+  // rectMode(CENTER);
+  // rect(0,0,300,50);
   translate(width/2,height/2);
-  rotate(frameCount);
-  fill(0);
-  rectMode(CENTER);
-  rect(0,0,300,50);
+  strokeWeight(8);
+  ellipse(0,0, clockSize,clockSize);
+  strokeWeight(10);
+  strokeCap(SQUARE);
+
+  for(let i=0; i < 12; i++){
+    line(0,clockSize/2*0.95,0,clockSize/2*0.80);
+    rotate(360/60);
+  }
 
   pop();//return
-  rectMode(CENTER);
-  ellipse(width/2,height/2,500);
+  // rectMode(CENTER);
+  // ellipse(width/2,height/2,500);
 }
