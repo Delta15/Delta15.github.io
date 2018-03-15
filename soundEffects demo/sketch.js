37bb2a4c457c5aaca9eabfe8b260023aba3eff67
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-03-15 14:54:23 -0600

    save

--- soundEffects demo/sketch.js
+++ soundEffects demo/sketch.js
@@ -1,7 +1,15 @@
+let backgroundMusic;
+
+function preload() {
+  backgroundMusic = loadSound("sound&music/bensound-acousticbreeze.mp3");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255);
+  backgroundMusic.setVolume(0.6);
+  backgroundMusic.play();
 }
