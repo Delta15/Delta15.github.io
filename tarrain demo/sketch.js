a800d97f44d4c166f465dc6af7029ddea54f466e
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-03-19 15:10:35 -0600

    add

--- tarrain demo/sketch.js
+++ tarrain demo/sketch.js
@@ -1,8 +1,26 @@
-
+let heights = [];
+let numberOfrect;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  numberOfrect = width;
 }
 
 function draw() {
   background(255);
+  displayTerrain();
+}
+
+function generateTerrain(numberofRect){
+  for (let i=0; i<numberofRect; i++){
+    heights.push(random(100, 500));
+  }
+}
+
+function displayTerrain(){
+  let rectWidth = width/numberOfrect;
+  rectMode(CORNER);
+  fill(0);
+  for(let i=0; i<numberOfrect; i++){
+    rect(0,height, rectWidth, height - height[0]);
+  }
 }
