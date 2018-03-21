a848a38f98ab2e6633ad8d1c30365f66ea35c20d
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-03-21 14:26:26 -0600

    save

--- soundEffects demo/sketch.js
+++ soundEffects demo/sketch.js
@@ -1,15 +1,18 @@
 let backgroundMusic;
 
 function preload() {
-  backgroundMusic = loadSound("sound&music/Map.wav");
+  backgroundMusic = loadSound("music/mapV.wav");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundMusic.setVolume(0.6);
-  backgroundMusic.play();
+  backgroundMusic.loop();
 }
 
 function draw() {
-  //background(255);
+  background(255,0,0);
+  textAlign(CENTER,CENTER);
+  textSize(100);
+  text(str,x,y,[x2],[y2])
 }
