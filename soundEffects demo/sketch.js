3c14c897a8325f46b7b8619d44acb3b40d472e46
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-03-22 14:55:50 -0600

    save

--- soundEffects demo/sketch.js
+++ soundEffects demo/sketch.js
@@ -1,19 +1,30 @@
 let backgroundMusic;
+let anotherMusic;
+let coder;
 
 function preload() {
   backgroundMusic = loadSound("music/mapV.wav");
+  anotherMusic = loadSound("music/Jingle.wav");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundMusic.setVolume(0.6);
+  anotherMusic.setVolume(0.6);
   backgroundMusic.loop();
+  coder = 0;
 }
 
 function draw() {
-  background(255,0,0);
+  background(coder);
   textAlign(CENTER,CENTER);
   textSize(100);
   fill(255);
   text("Sample",width/2,height/2);
+}
+
+function mouseClicked(){
+  backgroundMusic.stop();
+  anotherMusic.play();
+  coder = 255;
 }
