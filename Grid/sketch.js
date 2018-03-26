a54ee56dbdeea11139c8cdab0a05fd6345638007
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-03-26 14:53:55 -0600

    save

--- Grid/sketch.js
+++ Grid/sketch.js
@@ -24,6 +24,18 @@ function displayGrid(){
   }
 }
 
-function createRandom2DArray(){
-
+function createRandom2DArray(cols, rows){
+  let randomGrid = [];
+  for (let x=0; x<cols; x++){
+    randomGrid.push([]);
+    for(let y=0; y<rows; y++){
+      if (random(100) < 50){
+        randomGrid[x].push(0);
+      }
+      else {
+        randomGrid[x].push(1);
+      }
+    }
+  }
+  return randomGrid;
 }
