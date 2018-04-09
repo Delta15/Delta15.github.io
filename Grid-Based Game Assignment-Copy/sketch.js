2cc914ec6c5620af6f9d0d2702771e6c43f992da
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-04-09 15:01:55 -0600

    save

--- Grid-Based Game Assignment-Copy/sketch.js
+++ Grid-Based Game Assignment-Copy/sketch.js
@@ -1,6 +1,10 @@
 // McRavenTuazon
 // 4/9/2018
-// 
+//
+let rows = 3;
+let cols = 3;
+let grid;
+let cellS;
 
 function preload() {
 
@@ -8,8 +12,25 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cellS = width/ cols;
+  // grid = random2dArray(cols,rows);
 }
 
 function draw() {
-  background(0);
+  background(255);
+  drawGrid();
+}
+
+function drawGrid(){
+  for (let x=0; x<cols; x++){
+    for (let y=0; y<rows; y++){
+      if (grid[x][y]===0) {
+        fill(0);
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellS, y*cellS, cellS, cellS);
+    }
+  }
 }
