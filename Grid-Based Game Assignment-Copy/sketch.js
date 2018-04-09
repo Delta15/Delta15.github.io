53ce98684b222c2a0a7dfbb8662a52e31a4cfb74
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-04-09 15:12:36 -0600

    save

--- Grid-Based Game Assignment-Copy/sketch.js
+++ Grid-Based Game Assignment-Copy/sketch.js
@@ -6,18 +6,18 @@ let cols = 3;
 let grid;
 let cellS;
 
-function preload() {
-
-}
+// function preload() {
+//
+// }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 500);
   cellS = width/ cols;
-  // grid = random2dArray(cols,rows);
+  grid = Random2dArray(cols,rows);
 }
 
 function draw() {
-  background(255);
+  background(0);
   drawGrid();
 }
 
@@ -25,12 +25,48 @@ function drawGrid(){
   for (let x=0; x<cols; x++){
     for (let y=0; y<rows; y++){
       if (grid[x][y]===0) {
-        fill(0);
+        fill(255);
       }
       else {
-        fill(255);
+        fill(255,0,0);
       }
       rect(x*cellS, y*cellS, cellS, cellS);
     }
   }
+}
+
+function mousePressed() {
+  let xcoord = floor(mouseX / cellS);
+  let ycoord = floor(mouseY / cellS);
+
+  if (grid[xcoord][ycoord] === 1) {
+    grid[xcoord][ycoord] = 0;
+  }
+  else {
+    grid[xcoord][ycoord] = 1;
+  }
+}
+
+function keyPressed() {
+  grid = Random2dArray(cols, rows);
+}
+
+function Random2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x=0; x<cols; x++) {
+    randomGrid.push([]);
+    for (let y=0; y<rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(0);
+      }
+      else {
+        randomGrid[x].push(1);
+      }
+    }
+  }
+  return randomGrid;
+}
+
+function keyPressed(){
+  
 }
