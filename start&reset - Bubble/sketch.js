bb80095f5f1726ca524b87d59d159a05bf56e9b4
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-04-17 15:11:10 -0600

    save

---
+++ start&reset - Bubble/sketch.js
@@ -0,0 +1,43 @@
+let myTimer;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  myTimer = new Timer(1000);
+}
+
+function draw() {
+  if (myTimer.isDone()) {
+    ellipse(random(width), random(height), random(25, 100), random(25, 100));
+    myTimer.reset(100);
+  }
+}
+
+class Timer {
+  constructor(waitTime) {
+    this.waitTime = waitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  reset(newWaitTime) {
+    this.waitTime = newWaitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  isDone() {
+    if (millis() >= this.finishTime) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
+
+class Bubble {
+  constructor(){
+    
+  }
+  ellipse(random(width), random(height), random(100, 100), random(100, 100));
+}
