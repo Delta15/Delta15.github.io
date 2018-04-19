1f4bb72434ee31ff9a8d614897a25165241dbf8e
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-04-19 15:17:22 -0600

    save

--- Pair Programming Game Creation Assignment/sketch.js
+++ Pair Programming Game Creation Assignment/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  backgroundSetup();
 }
 
 function draw() {
-  background(255);
+  mainMenu();
+}
+
+function backgroundSetup() {
+  let goldColor = color(255, 198, 0);
+  let blueColor = color(0, 102, 153);
+  let mainMenuBackground = lerpColor(goldColor, blueColor, 0.50);
+  background(mainMenuBackground);
+}
+
+function mainMenu() {
+  textAlign(CENTER, CENTER);
+  fill(0);
+  textSize(100);
+  text("SAMPLE", width / 2, height / 2);
 }
