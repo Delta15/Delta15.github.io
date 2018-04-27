692bff08320bcbcff3f52d4745d24eeebf1b7532
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-04-27 12:05:03 -0500

    save

--- clicker/sketch.js
+++ clicker/sketch.js
@@ -1,56 +1,70 @@
 //Clicker
 //McRaven Tuazon
 //2/15/2018
-let drawBox;
-let drawBox2;
-let drawBox3;
-let drawBox4;
+let state = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  drawBox = true;
-  drawBox2 = true;
-  drawBox3 = true;
-  
+
 }
 
 function draw() {
-  background(255,105,180);
-  boxStart3();
-  boxStart();
-  boxStart2();
+  if (state === 1) {
+    background(255,105,180);
+    boxStart3();
+    boxStart();
+    boxStart2();
+  }
+  else if (state === 2) {
+    background(255,0,0);
+    boxStart3();
+    boxStart2();
+  }
+  else if (state === 3) {
+    background(0,255,0);
+    boxStart3();
+    boxStart();
+  }
+  else if (state === 4) {
+    background(0,0,255);
+    boxStart();
+    boxStart2();
+  }
 }
 
 function boxStart(){
-  if (drawBox === true){
-    noFill();
-    stroke(0);
-    rotateY(millis() / 800);
-    box(300, 300);
-  }
+  noFill();
+  stroke(0);
+  rotateY(millis() / 800);
+  box(300, 300);
 }
 
 function boxStart2(){
-  if (drawBox2 === true){
-    noFill();
-    stroke(0);
-    rotateX(millis() / 800);
-    box(200, 200);
-  }
+  noFill();
+  stroke(0);
+  rotateX(millis() / 800);
+  box(200, 200);
 }
 
 function boxStart3(){
-  if (drawBox3 === true){
-    fill(255);
-    noStroke();
-    rotateZ(millis() / 800);
-    box(100, 100);
-  }
+  fill(255);
+  stroke(0);
+  rotateZ(millis() / 800);
+  box(100, 100);
 }
 
 function mouseClicked(){
-  if (mouseButton === LEFT){
-    drawBox = false();
+  if (state === 1) {
+    state = 2;
+  }
+  else if (state === 2) {
+    state = 3;
+  }
+  else if (state === 3) {
+    state = 4;
+  }
+  else if (state === 4) {
+    state = 1;
   }
 }
 
@@ -62,7 +76,5 @@ function keyTyped(){
 }
 
 function deviceShaken(){
-  noStroke();
-  fill(random(255),random(255),random(255),random(255));
-  rect(random(50,100),random(50, 100),random(50, 100),random(50, 100));
+  state = 2;
 }
