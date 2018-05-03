aca80ed247780bf2abe59a5f7d540b58df81038e
Tuazon1203570 <1203570@sps-student.sk.ca>
2018-05-03 14:27:00 -0600

    save

--- Pair Programming Game Creation Assignment/sketch.js
+++ Pair Programming Game Creation Assignment/sketch.js
@@ -1,23 +1,26 @@
+//-------------------------------------
+// [MINI]
+// McRaven Tuazon and William Ahiahonu
+// Computer Science 30
+// Pair Programming Assignment
+// May 3rd, 2018
+//-------------------------------------
+
 let nameIntro, classIntro;
 let mainMusic;
-let gameOn, Gmusic, startT;
+let gameOn, Gmusic, startT, breathingSpace;
 let programState;
 let introSound, introSET, introGO, introSTART;
+let game1Timer, game2Timer, overallTimer;
 let textDisplay = " ";
 let fadeAnimation;
+
+// Game one global variables.
 let rects = [];
 let numRects = 50;
 let cir;
 
 // #################################################################################################
-//---------------------------------
-// Stickman Game - William Ahiahonu
-// Component of MINI Arcade
-// Computer Science 30
-// Pair Programming Assignment
-// May 2nd, 2018
-//---------------------------------
-
 // Stickman game global variables - William.
 let stickman, collisionDetector;
 let stickmanGameImage, stickmanStandState, stickmanJumpState, stickmanRunState1, stickmanRunState2,
@@ -43,19 +46,24 @@ function preload() {
   lavaImage = loadImage("assets/lavaimage.png");
   // ###############################################################################################
 }
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   mainMusic.loop();
   programState = 1;
   nameIntro = new Timer(3900);
   fadeAnimation = createGraphics(windowWidth, windowHeight);
-  for (i = 0; i < numRects; i++) {
-    r = new rectObj(random(width), random(height), random(10, 50), random(10, 50));// generate a rectObj
+  game1Timer = 30;
+  game2Timer = 30;
+  overallTimer = 123;
+
+  // ###############################################################################################
+  // Game 1 setup.
+  for (let i = 0; i < numRects; i++) {
+    let r = new rectObj(random(width), random(height), random(10, 80), random(10, 50));// generate a rectObj
     rects.push(r); //add it to the array.
   }
   cir = new circleObj(20); // create a new circle object
-  console.log(rects);
+  window.console.log(rects);
 
   // ###############################################################################################
   // Stickman game setup tools - William.
@@ -73,6 +81,8 @@ function setup() {
   // game and highscore control - William.
   score = 0;
   highScore = 0;
+  // #################################################################################################
+
   // Checking for a local storage value from the last time played, else
   // the value is set beginning with the first game played - William.
   if (!localStorage.getItem("highscore")) {
@@ -83,9 +93,7 @@ function setup() {
     // Retrieve the highscore if it already exists - William.
     getHighScore();
   }
-  // #################################################################################################
 }
-
 function draw() {
   if (programState === 1) {
     background(0);
@@ -93,8 +101,10 @@ function draw() {
     textAlign(CENTER, CENTER);
     textSize(100);
     text("McRavenTuazon", width / 2 - 200, height / 2 - 50);
+    push();
     rectMode(CENTER);
     rect(width / 2, height / 2 + 50, windowWidth, 100);
+    pop();
     fill(0);
     text("WilliamAhiahonu", width / 2 + 200, height / 2 + 50);
     if (nameIntro.isDone()) {
@@ -136,89 +146,89 @@ function draw() {
     if (introSTART.isDone()) {
       programState = 7;
       gameOn = new Timer(1000);
+      breathingSpace = new Timer(2000);
       Gmusic.play();
     }
   }
   else if (gameOn.isDone() && programState === 7) {
-    game1();
-    noCursor();
+    background(0);
+    cir.disp(mouseX, mouseY); //pass the x,y pos in to the circle.
+    if (breathingSpace.isDone()) {
+      game1();
+      noCursor();
+    }
+    // Tracking the score - William.
+    scoreTracker();
+    overallGameTimer();
+    gameOneTimer();
   }
   else if (programState === 8) {
-    cursor();
-    background(255);
-    // #################################################################################################
     noStroke();
     if (gameState === 1) {
     // Stickman game function, containing all of the components of it - William.
       stickManGame();
+      // Tracking the score - William.
+      scoreTracker();
+      gameTwoTimer();
+      overallGameTimer();
     }
     else if (gameState === 2) {
-    // If stickman hits an obstacle, game over - William.
-      gameOverConditionals();
+      programState = 7;
+      breathingSpace = new Timer(1000);
+      // Obstacles are pushed to the end of the screen opposite of the stickman
+      // before the game is rerun - William.
+      obstacleX = windowWidth;
+      // Game state switched back to 1 - William.
+      gameState = 1;
     }
-  // #################################################################################################
-
   }
-
-  function game1() {
-    background(0);
-    for (i = 0; i < numRects; i++) {
-      rects[i].disp();
-      rects[i].collide(cir); //collide against the circle object
-    }
-
-    cir.disp(mouseX, mouseY); //pass the x,y pos in to the circle.
+  else if (programState === 9) {
+    gameOverConditionals();
   }
 }
 
-function rectObj(x, y, w, h) {
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
-  this.color = color(255, 0, 0);
-  this.hit = false;
-
-  this.collide = function(obj) {
-
-    this.hit = collideRectCircle(this.x, this.y, this.w, this.h, obj.x, obj.y, obj.dia); //collide the cir object into this rectangle object.
-
-    if (this.hit) {
-      // this.color = color(0); //set this rectangle to be black if it gets hit
-      programState = 8;
-    }
-
-  };
-
-  this.disp = function() {
-    noStroke();
-    fill(this.color);
-    this.y += 3; //move down!;
-    if (this.y > height) { //loop to the left!
-      this.y = -this.h;
-    }
-    rect(this.x, this.y, this.w, this.h);
-
-  };
-
+// Function to set local storage value to highscore in the game - William.
+function setHighScore() {
+  localStorage.setItem("highscore", score);
 }
-
-function circleObj(dia) {
-  this.dia = dia;
-  this.color = color(255);
-  this.x;
-  this.y;
-
-  this.disp = function(x, y) {
-    this.x = x;
-    this.y = y;
-    noStroke();
-    fill(this.color);
-    ellipse(this.x, this.y, this.dia, this.dia);
-  };
-
+// Function to get local storage value if possible - William.
+function getHighScore() {
+  highScore = localStorage.getItem("highscore");
 }
-
+// Function to remove local storage value, if you can't beat the highscore :) - William.
+function keyTyped() {
+  if (key === "c") {
+    localStorage.removeItem("highscore");
+  }
+}
+function scoreTracker() {
+  // Display for score and highscore within the game - William.
+  push();
+  if (programState === 7) {
+    fill(0);
+  }
+  else if (programState === 8) {
+    fill(255);
+  }
+  rect(0, 0, windowWidth, 120);
+  textSize(30);
+  if (programState === 7) {
+    fill(255);
+  }
+  else if (programState === 8) {
+    fill(0);
+  }
+  push();
+  textFont("impact");
+  textAlign(CENTER);
+  text("Score: " + score + " points", windowWidth/2+480, 30);
+  text("High Score: " + highScore + " points", windowWidth/2+480, 80);
+  pop();
+  push();
+  textSize(90);
+  text("MINI", windowWidth/2, 60);
+  pop();
+}
 function mainMenu() {
   background(255);
   push();
@@ -251,12 +261,21 @@ function instract(){
   background(255);
   textAlign(CENTER,CENTER);
   fill(0);
+  rect(0, 0, windowWidth, windowHeight/2 - 250);
+  fill(255);
   textSize(50);
-  text("I", width/2, height/2 - 300);
+  text("GAME I", width/2, height/2 - 300);
+  fill(0);
   text("Use mouse to control white circle", width/2, height/2 - 200);
   text("Avoid red rectangles", width/2, height/2 - 100);
-  text("II", width/2, height/2);
-  text("SAMPLE", width/2, height/2 + 100);
+  rect(0, windowHeight/2-50, windowWidth, 100);
+  fill(255);
+  text("GAME II", width/2, height/2);
+  fill(0);
+  text("Up Arrow (small jump), X (big jump), Right Arrow (Run)", width/2, height/2 + 100);
+  text("Avoid incoming obstacles", width/2, height/2 + 200);
+  rect(0, windowHeight/2 + 250, windowWidth, 105);
+  fill(255);
   text("CLICK TO CONTINUE", width/2, height/2 + 300);
   if (mouseIsPressed) {
     programState = 5;
@@ -268,42 +287,102 @@ function instract(){
 function introBG() {
   background(255);
   noStroke();
+  push();
   rectMode(CENTER);
   fill(0);
   rect(width / 2, height / 2, 500, windowHeight);
   fill(255);
   rect(width / 2, height / 2, 480, windowHeight);
+  pop();
   fill(0);
   textAlign(CENTER, CENTER);
   textSize(100);
   text(textDisplay, width / 2, height / 2);
 }
+// ###################################################################################################
+function game1() {
+  background(0);
+  for (let i = 0; i < numRects; i++) {
+    rects[i].disp();
+    rects[i].collide(cir); //collide against the circle object
+  }
+  cir.disp(mouseX, mouseY); //pass the x,y pos in to the circle.
+  // Display score and highscore - William.
+  scoreTracker();
+}
 
-class Timer {
-  constructor(waitTime) {
-    this.waitTime = waitTime;
-    this.startTime = millis();
-    this.finishTime = this.startTime + this.waitTime;
-    this.timerIsDone = false;
-  }
-  reset(newWaitTime) {
-    this.waitTime = newWaitTime;
-    this.startTime = millis();
-    this.finishTime = this.startTime + this.waitTime;
-    this.timerIsDone = false;
-  }
-  isDone() {
-    if (millis() >= this.finishTime) {
-      return true;
+function rectObj(x, y, w, h) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.color = color(255, 0, 0);
+  this.hit = false;
+
+  this.collide = function(obj) {
+
+    this.hit = collideRectCircle(this.x, this.y, this.w, this.h, obj.x, obj.y, obj.dia); //collide the cir object into this rectangle object.
+
+    if (this.hit) {
+      programState = 8;
+      game1Timer = 30;
     }
-    else {
-      return false;
+  };
+
+  this.disp = function() {
+    noStroke();
+    fill(this.color);
+    this.y += 5; //move down!;
+    if (this.y > height) { //loop downwards!
+      this.y = -this.h;
+    }
+    rect(this.x, this.y, this.w, this.h);
+
+  };
+}
+
+function circleObj(dia) {
+  this.dia = dia;
+  this.color = color(255);
+  this.x;
+  this.y;
+
+  this.disp = function(x, y) {
+    this.x = x;
+    this.y = y;
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.dia, this.dia);
+  };
+}
+// Game one timer.
+function gameOneTimer() {
+  push();
+  textSize(30);
+  fill(255);
+  push();
+  textFont("impact");
+  textAlign(CENTER);
+  text("Time left in Game One: " + game1Timer + " s", windowWidth/2-450, 80);
+  pop();
+  if (programState === 7) {
+    if (frameCount % 60 === 0) {
+      game1Timer --;
+      score ++;
+      if (score > highScore) {
+        setHighScore();
+        highScore = score;
+      }
+    }
+    if (game1Timer === 0) {
+      programState = 8;
+      game1Timer = 30;
     }
   }
 }
 // ###################################################################################################
 function stickManGame() {
-  // Background Image.
+  // Background Image - William.
   push();
   imageMode(CORNER);
   image(stickmanGameImage, 0, 0, windowWidth, windowHeight+150);
@@ -323,8 +402,6 @@ function stickManGame() {
   stickman.updateDisplay();
   stickman.newPositionAndSurfaceDetection();
   stickman.jump(-7.8);
-  // Tracking the score - William.
-  scoreTracker();
   // Disabling spacebar functionality so the window does not scroll up or down
   // when the spacebar is pressed - William.
   window.onkeydown = function killSpaceBar() {
@@ -337,56 +414,28 @@ function stickManGame() {
   // Adding listener to disable scrolling - - William.
   window.addEventListener("scroll", disableScrolling);
 }
-function scoreTracker() {
-  // Display for score and highscore within the game - William.
+// Game two timer.
+function gameTwoTimer() {
   push();
-  textSize(50);
+  textSize(30);
   fill(0);
   push();
   textFont("impact");
   textAlign(CENTER);
-  text("Score: " + score + " m", windowWidth/2, 70);
-  text("High Score: " + highScore + " m", windowWidth/2, 150);
+  text("Time left in Game Two: " + game2Timer + " s", windowWidth/2-450, 80);
   pop();
-}
-// Function to set local storage value to highscore in the game - William.
-function setHighScore() {
-  localStorage.setItem("highscore", score);
-}
-// Function to get local storage value if possible - William.
-function getHighScore() {
-  highScore = localStorage.getItem("highscore");
-}
-// Function to remove local storage value, if you can't beat the highscore :) - William.
-function keyTyped() {
-  if (key === "c") {
-    localStorage.removeItem("highscore");
+  if (programState === 8) {
+    if (frameCount % 60 === 0) {
+      game2Timer --;
+    }
+    if (game2Timer === 0) {
+      programState = 7;
+      game2Timer = 30;
+      breathingSpace = new Timer(1000);
+    }
   }
 }
-function gameOverConditionals() {
-  // Display final score after stickman is dead - William.
-  background(0);
-  push();
-  fill(255);
-  textSize(90);
-  textAlign(CENTER);
-  textFont("impact");
-  fill("red");
-  text("YOU DIED!", windowWidth/2, windowHeight/2);
-  fill("gold");
-  textSize(40);
-  text("Final Score: " + score + " m", windowWidth/2, windowHeight/2+70);
-  text("High Score: " + highScore + " m", windowWidth/2, windowHeight/2+140);
-  pop();
-  // Obstacles are pushed to the end of the screen opposite of the stickman
-  // before the game is rerun to give the player time to react - William.
-  obstacleX = windowWidth;
-  // Change...
-  if(keyIsDown(82)) { // key r
-    gameState = 1;
-    score = 0;
-  }
-}
+// Specific class for the stick man and its behaviour - William.
 class StickmanCharacter {
   constructor(objectX) {
     this.width = width;
@@ -466,6 +515,9 @@ class StickmanCharacter {
     }
   }
 }
+// Specific class for the collision detector (hitbox)
+// that follows the movements of the stickman. The class also holds the
+// obstacles. - William.
 class CollisionDetection {
   constructor(objectX) {
     this.width = width;
@@ -477,7 +529,6 @@ class CollisionDetection {
     this.gravitySpeed = 0;
   }
   updateDisplay() {
-    //fill("grey");
     // Hitbox.
     rect(this.objectX-36, this.objectY-92, 60, 150);
   }
@@ -576,7 +627,75 @@ class CollisionDetection {
     }
     if (this.obstacleOccurance || this.obstacleOccurance3) {
       gameState = 2;
+      game2Timer = 30;
     }
   }
 }
 // ###################################################################################################
+
+function overallGameTimer() {
+  push();
+  textSize(30);
+  if (programState === 7) {
+    fill(255);
+  }
+  else if (programState === 8) {
+    fill(0);
+  }
+  push();
+  textFont("impact");
+  textAlign(CENTER);
+  text("Overall Time: " + overallTimer + " s", windowWidth/2-480, 30);
+  pop();
+  if (frameCount % 60 === 0) {
+    overallTimer --;
+  }
+  if (overallTimer === 0) {
+    programState = 9;
+    overallTimer = 123;
+  }
+}
+function gameOverConditionals() {
+  // Display final score after time is up - William.
+  background(0);
+  push();
+  fill(255);
+  textSize(90);
+  textAlign(CENTER);
+  textFont("impact");
+  fill("red");
+  text("TIME'S UP!", windowWidth/2, windowHeight/2);
+  fill("gold");
+  textSize(40);
+  text("Final Score: " + score + " points", windowWidth/2, windowHeight/2+70);
+  text("High Score: " + highScore + " points", windowWidth/2, windowHeight/2+140);
+  pop();
+  // Change...
+  if(keyIsDown(82)) { // key r
+    programState = 3;
+    score = 0;
+  }
+}
+// Universal class for the overall program.
+class Timer {
+  constructor(waitTime) {
+    this.waitTime = waitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  reset(newWaitTime) {
+    this.waitTime = newWaitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  isDone() {
+    if (millis() >= this.finishTime) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
