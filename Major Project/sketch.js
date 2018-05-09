9c941254df08d8402195d9416833301d853a4683
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-05-09 10:39:08 -0500

    save

--- Major Project/sketch.js
+++ Major Project/sketch.js
@@ -1,44 +1,67 @@
 let state = 1;
-let timerONE, timerTWO, timerTHREE;
+let timerONE, timerTWO, timerTHREE, timerFOUR;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  timerONE = new Timer(900);
+  timerONE = new Timer(300);
 }
 
 function draw() {
   if (state === 1) {
-    mainManu();
+    display();
     if (timerONE.isDone()) {
       state = 2;
-      timerTWO = new Timer(700);
+      timerTWO = new Timer(300);
     }
   }
   else if (state === 2) {
-    intro();
+    display2();
     if (timerTWO.isDone()) {
       state = 3;
-      timerTHREE = new Timer(700);
+      timerTHREE = new Timer(300);
     }
   }
   else if (state === 3) {
-    mainManu();
+    display3();
     if (timerTHREE.isDone()) {
+      state = 4;
+      timerFOUR = new Timer(300);
+    }
+  }
+  else if (state === 4) {
+    display4();
+    if (timerFOUR.isDone()) {
       state = 1;
-      timerONE = new Timer(700);
+      timerONE.reset(0);
     }
   }
 }
 
-function mainManu(){
+function display(){
   background(0);
   textAlign(CENTER,CENTER);
   fill(255);
   textSize(100);
   text("SAMPLE",width/2,height/2);
 }
-function intro(){
+function display2(){
   background(255,0,0);
+  textAlign(CENTER,CENTER);
+  fill(0);
+  textSize(100);
+  text("SAMPLE",width/2,height/2);
+}
+
+function display3(){
+  background(0,255,0);
+  textAlign(CENTER,CENTER);
+  fill(255);
+  textSize(100);
+  text("SAMPLE",width/2,height/2);
+}
+
+function display4(){
+  background(255);
   textAlign(CENTER,CENTER);
   fill(0);
   textSize(100);
